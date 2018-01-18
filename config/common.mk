nitrogen-project/android_vendor_nitrogen

add support for building custom themes

products/common.mk
@@ -91,3 +91,6 @@ PRODUCT_PACKAGES += \
 
 # Boot animations
 $(call inherit-product-if-exists, vendor/nitrogen/products/bootanimation.mk)
+
+# Themes
+$(call inherit-product-if-exists, vendor/nitrogen/products/themes.mk)

venkysai/android_vendor_liquid

config/common.mk
@@ -97,6 +97,9 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     vendor/liquid/prebuilt/common/media/bootanimation.zip:system/media/bootanimation.zip
 
+# Themes
+$(call inherit-product-if-exists, vendor/liquid/products/themes.mk)
+
 # TWRP
 ifeq ($(WITH_TWRP),true)
 include vendor/liquid/config/twrp.mk