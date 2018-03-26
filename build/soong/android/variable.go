19d004b60b79f92e716e7974fd26f8e670752610
liquid0624 <rjbirnesser@gmail.com>
2018-03-26 15:22:33 -0400

    Revert "soong: Add BOARD_USES_QCOM_HARDWARE"
    This reverts commit fad68a88401e19b550efb95fedafaa75b77de151.

--- build/soong/android/variable.go
+++ build/soong/android/variable.go
@@ -12,10 +12,6 @@ type Product_variables struct {
 	Uses_qcom_bsp_legacy struct {
 		Cppflags []string
 	}
-	Uses_qcom_hardware struct {
-		Cppflags []string
-		Shared_libs []string
-	}
 }
 
 type ProductVariables struct {
@@ -24,5 +20,4 @@ type ProductVariables struct {
 	Target_shim_libs  *string `json:",omitempty"`
 	Uses_generic_camera_parameter_library  *bool `json:",omitempty"`
 	Uses_qcom_bsp_legacy  *bool `json:",omitempty"`
-	Uses_qcom_hardware  *bool `json:",omitempty"`
 }
